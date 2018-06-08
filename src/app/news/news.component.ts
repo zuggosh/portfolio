9fda4fe1d75e75425a9ae19863cf8fe3ae8a03da
LimanskiyY.S <limanskiy1989@gmail.com>
2018-06-09 01:21:50 +0300

    add style for news page

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { News, NewsAssets, NewsService } from '../service/news/news.service';
 import { Observable, Subject, pipe } from 'rxjs';
 
-import {debounceTime} from 'rxjs/operators'
+import {debounceTime} from 'rxjs/operators';
 
 @Component({
   selector: 'app-news',
@@ -27,13 +27,13 @@ export class NewsComponent implements OnInit {
     this.querySearchRequest.pipe(
         debounceTime (1000)
     ).subscribe((value) => {
-        if(value !== 0){
+        if (value !== 0) {
             this.querySearch = `&q=${value}`;
-        }else {
+        } else {
             this.querySearch = '';
         }
         this.showPressProof(this.country, this.category, this.querySearch);
-    })
+    });
   }
 
   changePressProofCountry(countryCode) {
@@ -41,6 +41,13 @@ export class NewsComponent implements OnInit {
     this.showPressProof(this.country, this.category, this.querySearch);
   }
   changePressProofCategory(category) {
+      this.newsAssets.categories.forEach(function (item, index) {
+          if (category === item.title) {
+              item.active = true;
+          } else {
+              item.active = false;
+          }
+      });
       this.category = category;
       this.showPressProof(this.country, this.category, this.querySearch);
   }
--- src/app/service/news/news.service.ts
+++ src/app/service/news/news.service.ts
@@ -6,7 +6,7 @@ export interface News {
 }
 
 export interface NewsAssets {
-
+    categories: any;
 }
 
 @Injectable({
