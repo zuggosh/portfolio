978bb6638e76ba33bf66367fc2cfbc7d68d2d114
LimanskiyY.S <limanskiy1989@gmail.com>
2018-06-09 16:45:48 +0300

    scroll element on about and fix bug with active router

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.scss']
 })
+
+
 export class AboutMeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+  @HostListener("window:scroll", ['$event'])
+  onWindowScroll() {
+      let topBuffet = 0;
+      const el = document.getElementById('aboutMeNav');
+      const aboutMe = document.getElementById('aboutMe').getBoundingClientRect().top;
+      if (aboutMe < 0) {
+          topBuffet = 50;
+          el.style.top = Math.abs(aboutMe) + topBuffet + 'px';
+      }
+  }
+  slideToElement(element) {
+    document.getElementById(element).scrollIntoView({behavior: "smooth"});
   }
 
 }
