49a8acbf51604eaec3013664f1b2d19a514b5e8a
LimanskiyY.S <limanskiy1989@gmail.com>
2018-06-12 23:14:52 +0300

    add model to component for compile to prod

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
 
+
+class AppComponentModel {
+    show: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+    appComponentModel = new AppComponentModel();
     scrollHandler($event) {
         let topBuffet = 0;
         const aboutMe = document.getElementById('aboutMe').getBoundingClientRect().top;
