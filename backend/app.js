941e2864685759ff84c907118b75d477dd006f6b
LimanskiyY.S <limanskiy1989@gmail.com>
2018-06-17 08:25:58 +0300

    add url to get profile info

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next)=>{
-    res.send('hello pidr');
+app.use('/api/post',(req, res, next)=>{
+    const post = {
+      name: 'Yakov Limansiy'
+    };
+    res.json({
+        message: 'Post get successfully',
+        post: post
+    })
 });
 
 module.exports = app;
