65dbdadd6183212405af2abad5d9bab1db0415c1
LimanskiyYS <limanskiy1989@gmail.com>
2018-12-25 22:21:42 +0200

    add mock data in proj // connect comp and serv

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,20 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { FetchDataService } from '../service/fetch-data/fetch-data.service';
 
+import { FetchDataService } from '../service/fetch-data/fetch-data.service';
+import { PortfolioService, PortfolioData } from '../service/portfolio/portfolio.service';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
+  providers: [PortfolioService]
 })
 export class PortfolioComponent implements OnInit {
     imageUrls: (any)[] = [
-        { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56748793/dbohn_170625_1801_0018.0.0.jpg', caption: 'The first slide', href: '#config' },
-        { url: 'https://cdn.vox-cdn.com/uploads/chorus_asset/file/9278671/jbareham_170917_2000_0124.jpg', clickAction: () => alert('custom click function') },
-        { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56789263/akrales_170919_1976_0104.0.jpg', caption: 'Apple TV', href: 'https://www.apple.com/' }
+        { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56748793/dbohn_170625_1801_0018.0.0.jpg' },
+        { url: 'https://cdn.vox-cdn.com/uploads/chorus_asset/file/9278671/jbareham_170917_2000_0124.jpg' },
+        { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56789263/akrales_170919_1976_0104.0.jpg' }
     ];
     height: string = '400px';
-    minHeight: string;
     arrowSize: string = '30px';
     showArrows: boolean = true;
     disableSwiping: boolean = false;
@@ -33,11 +34,22 @@ export class PortfolioComponent implements OnInit {
     lazyLoad: boolean = false;
     hideOnNoSlides: boolean = false;
     width: string = '100%';
-
-    constructor(private fetchDataService: FetchDataService) { }
+    portfolioData: PortfolioData;
+    constructor(private fetchDataService: FetchDataService,
+                private portfolioService: PortfolioService) { }
 
     ngOnInit() {
+      // console.log(this.portfolioService.getPortfolioAssets());
       // this.fetchDataService.changeMessage(false);
+      this.getPortfolioData();
+    }
+
+    getPortfolioData() {
+      this.portfolioService.getPortfolioData()
+        .subscribe((data: PortfolioData) => {
+          this.portfolioData = data;
+          console.log(this.portfolioData);
+        });
     }
 
 }
