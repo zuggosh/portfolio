766d831eaec2792730af49107f30d1a1ce86040e
LimanskiyYS <limanskiy1989@gmail.com>
2018-12-28 22:55:03 +0200

    add pic in project

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -10,18 +10,11 @@ import { PortfolioService, PortfolioData } from '../service/portfolio/portfolio.
   providers: [PortfolioService]
 })
 export class PortfolioComponent implements OnInit {
-    imageUrls: (any)[] = [
-        { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56748793/dbohn_170625_1801_0018.0.0.jpg' },
-        { url: 'https://cdn.vox-cdn.com/uploads/chorus_asset/file/9278671/jbareham_170917_2000_0124.jpg' },
-        { url: 'https://cdn.vox-cdn.com/uploads/chorus_image/image/56789263/akrales_170919_1976_0104.0.jpg' }
-    ];
     height: string = '400px';
     arrowSize: string = '30px';
     showArrows: boolean = true;
     disableSwiping: boolean = false;
-    autoPlay: boolean = true;
-    autoPlayInterval: number = 3333;
-    stopAutoPlayOnSlide: boolean = true;
+    autoPlay: boolean = false;
     debug: boolean = false;
     backgroundSize: string = 'cover';
     backgroundPosition: string = 'center center';
@@ -39,8 +32,6 @@ export class PortfolioComponent implements OnInit {
                 private portfolioService: PortfolioService) { }
 
     ngOnInit() {
-      // console.log(this.portfolioService.getPortfolioAssets());
-      // this.fetchDataService.changeMessage(false);
       this.getPortfolioData();
     }
 
