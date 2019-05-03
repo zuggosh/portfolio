6d3198e34f99e4e9f89ff271a8a6139dbb4dc89c
LimanskiyY.S <limanskiy1989@gmail.com>
2019-05-03 20:28:36 +0300

    commit all fitches

--- src/app/training/training.component.ts
+++ src/app/training/training.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { FetchDataService } from '../service/fetch-data/fetch-data.service';
+import { TrainingService } from '../service/training/training.service';
 
 @Component({
   selector: 'app-training',
@@ -8,8 +9,12 @@ import { FetchDataService } from '../service/fetch-data/fetch-data.service';
   styleUrls: ['./training.component.scss']
 })
 export class TrainingComponent implements OnInit {
+
+  private trainingData: object;
   public mockData: any;
-  constructor(private fetchDataService: FetchDataService) {
+
+  constructor(private fetchDataService: FetchDataService,
+              private trainingService: TrainingService) {
     this.mockData = [
       {
         title: 'chest',
@@ -33,10 +38,19 @@ export class TrainingComponent implements OnInit {
 
   ngOnInit() {
     this.fetchDataService.changeMessage(false);
+    this.getTrainingData();
   }
 
   changeValue(bool, i, j) {
     bool ? this.mockData[i].exercises[j].wight += 5 : this.mockData[i].exercises[j].wight -= 5;
+  }
+
+  getTrainingData() {
+    this.fetchDataService.changeMessage(true);
+    this.trainingService.getTrainingData().subscribe((data) => {
+      this.trainingData = data;
+      this.fetchDataService.changeMessage(false);
+    });
   }
 
 }
